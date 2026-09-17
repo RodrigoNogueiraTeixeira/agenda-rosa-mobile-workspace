98fc6ceafe67b6748e30e5c0eaee852ba3e1ffa9
Rodrigo Nogueira <digo22nt@gmail.com>
2026-09-17 01:49:27 -0300

    Estrutura back-end criada.

---
+++ back-end/mobile-api/src/test/java/com/agendarosa/MobileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendarosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
